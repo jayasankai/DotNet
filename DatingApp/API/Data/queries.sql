67fa88f4a3d1d633e3f95e69e9a731f19133034c
jayasanka <61721893+jayasanka-ucsc@users.noreply.github.com>
2023-12-15 09:19:04 +1300

    repo changed

---
+++ DatingApp/API/Data/queries.sql
@@ -0,0 +1,6 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Jayasanka"),
+(2, "Shanidi"),
+(3, "Amila"),
+(4, "Kumudu");
---
+++ DatingApp/API/Program.cs
@@ -0,0 +1,18 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultSqliteConnection"));
+});
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.MapControllers();
+
+app.Run();
