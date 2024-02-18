ebec2b67c763c9ec34be26d6a648bff8c0c09abd
jayasanka <61721893+jayasanka-ucsc@users.noreply.github.com>
2024-02-19 12:37:09 +1300

    Api versioning with ASP MVC

--- ShopApp/Program.cs
+++ ShopApp/Program.cs
@@ -1,10 +1,17 @@
 using ShopAppAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Asp.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options => {
+    options.ReportApiVersions = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+}).AddMvc();
+
 builder.Services.AddDbContext<ShopContext>(options => options.UseInMemoryDatabase("shop"));
 
 var app = builder.Build();
